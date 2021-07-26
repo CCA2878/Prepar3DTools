4b408bb5190844bfcf7cc151016aa3ae7b839a0a
CCA2878 <1054331608@qq.com>
2021-07-26 23:58:45 +0800

    2021-7-26 23:58_Trans func to derived class

--- function.py
+++ function.py
@@ -1,22 +1,26 @@
 import configparser
 from chardet.universaldetector import UniversalDetector
 
-def readCfg(cfgAddr = 'E:/scenery.cfg'):
-    _detector = UniversalDetector()
-    _cfg = configparser.ConfigParser()
-    _cfgFile = open(cfgAddr, 'rb')
 
-    for line in _cfgFile:
-        _detector.feed(line)
-        if _detector.done:
-            _cfgFile.close()
-            _detector.close()
-            break
+class readCfg(configparser.ConfigParser):
 
-    _cfg.read(cfgAddr, _detector.result['encoding'])
-    _detector.reset()
-    return _cfg
+    def __init__(self, cfgAddr='E:/scenery.cfg') -> None:
+        super().__init__()
+        _detector = UniversalDetector()
+        _cfgFile = open(cfgAddr, 'rb')
+
+        for line in _cfgFile:
+            _detector.feed(line)
+            if _detector.done:
+                _cfgFile.close()
+                _detector.close()
+                break
+
+        self.read(cfgAddr, _detector.result['encoding'])
+        _detector.reset()
+
 
 sceneryCfg1 = readCfg()
 sceneryCfg2 = readCfg('E:/scenery2.cfg')
 print(sceneryCfg1.sections(), sceneryCfg2.sections())
+print(sceneryCfg1.sections())
