1b81af1463cbabfa60d66e0e87ceb522c4f724b2
CCA2878 <1054331608@qq.com>
2021-07-27 01:03:33 +0800

    2021-7-27 01:02_Change names for standard

--- function.py
+++ function.py
@@ -1,26 +1,26 @@
 import configparser
 from chardet.universaldetector import UniversalDetector
 
-
-class readCfg(configparser.ConfigParser):
+# Judge cfg file encoding and read it.|判断文件编码方式并读取。
+class P3dCfgOpr(configparser.ConfigParser):
 
     def __init__(self, cfgAddr='E:/scenery.cfg') -> None:
         super().__init__()
-        _detector = UniversalDetector()
+        _cfgFileDetector = UniversalDetector()
         _cfgFile = open(cfgAddr, 'rb')
 
-        for line in _cfgFile:
-            _detector.feed(line)
-            if _detector.done:
+        for _line in _cfgFile:
+            _cfgFileDetector.feed(_line)
+            if _cfgFileDetector.done:
                 _cfgFile.close()
-                _detector.close()
+                _cfgFileDetector.close()
                 break
 
-        self.read(cfgAddr, _detector.result['encoding'])
-        _detector.reset()
+        self.read(cfgAddr, _cfgFileDetector.result['encoding'])
+        _cfgFileDetector.reset()
 
 
-sceneryCfg1 = readCfg()
-sceneryCfg2 = readCfg('E:/scenery2.cfg')
+sceneryCfg1 = P3dCfgOpr()
+sceneryCfg2 = P3dCfgOpr('E:/scenery2.cfg')
 print(sceneryCfg1.sections(), sceneryCfg2.sections())
 print(sceneryCfg1.sections())
