66117526429da0dc73ad2e2e55d1ef346c6f58bc
CCA2878 <1054331608@qq.com>
2021-07-29 02:23:29 +0800

    2021-7-29 02:23_New Func added

--- function.py
+++ function.py
@@ -1,28 +1,72 @@
 # -*- coding: utf-8 -*-
 
 import configparser
+from winreg import OpenKey, HKEY_CURRENT_USER, QueryValueEx
 from chardet.universaldetector import UniversalDetector
 
 # Judge cfg file encoding and read it.|判断文件编码方式并读取。
+
+
 class P3dCfgOpr(configparser.ConfigParser):
 
     def __init__(self, cfgAddr='E:/scenery.cfg') -> None:
         super().__init__()
-        _cfgFileDetector = UniversalDetector()
-        _cfgFile = open(cfgAddr, 'rb')
+        self.cfgAddr = cfgAddr
+        self.__getCfgEncoding()
+        self.read(self.cfgAddr, self.__cfgEncoding)
 
-        for _line in _cfgFile:
-            _cfgFileDetector.feed(_line)
-            if _cfgFileDetector.done:
-                _cfgFile.close()
-                _cfgFileDetector.close()
+    def __getCfgEncoding(self) -> None:
+        __cfgFileDetector = UniversalDetector()
+        __cfgFile = open(self.cfgAddr, 'rb')
+
+        for _line in __cfgFile:
+            __cfgFileDetector.feed(_line)
+            if __cfgFileDetector.done:
                 break
 
-        self.read(cfgAddr, _cfgFileDetector.result['encoding'])
-        _cfgFileDetector.reset()
+        __cfgFile.close()
+        __cfgFileDetector.close()
+        self.__cfgEncoding = __cfgFileDetector.result['encoding']
+        __cfgFileDetector.reset()
+
+
+class P3dInfoSrc():
+
+    def __init__(self, version='v5') -> None:
+        self.version = version
+        pass
+
+    @staticmethod
+    def getInstedVerList():
+        try:
+            __key = OpenKey(HKEY_CURRENT_USER, r'Software\Lockheed Martin')
+        except:
+            return []
+        else:
+            __key.Close()
+
+        __allVerList = ['v2', 'v3', 'v4', 'v5']
+        __instedVerList = []
+
+        for k in __allVerList:
+            try:
+                __key = OpenKey(HKEY_CURRENT_USER,
+                                r'Software\Lockheed Martin\Prepar3D ' + f'{k}')
+                if QueryValueEx(__key, 'Installed')[0] != 1:
+                    raise Exception('Installed Value is Not 1!')
+            except:
+                continue
+            else:
+                __key.Close()
+                __instedVerList.append(k)
+
+        return __instedVerList
+
+    def getInstAddr():
+        pass
 
 
 #sceneryCfg1 = P3dCfgOpr()
 #sceneryCfg2 = P3dCfgOpr('E:/scenery2.cfg')
 #print(sceneryCfg1.sections(), sceneryCfg2.sections())
-#print(sceneryCfg1.sections())
+# print(sceneryCfg1.sections())
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from function import P3dCfgOpr
+from function import P3dCfgOpr, P3dInfoSrc
 import sys
 from gui import *
 from PySide6.QtCore import *
@@ -12,28 +12,29 @@ if __name__ == '__main__':
     tableItemDict = {}
     tableItemDict = tableItemDict.fromkeys(columnContList, [])
 
-    sceneryCfgContent = P3dCfgOpr()
+    sceneCfgCont = P3dCfgOpr()
     app_MainWindow = QApplication(sys.argv)
     obj_MainWindow = QMainWindow()
     gui_MainWindow = Ui_MainWindow()
     gui_MainWindow.setupUi(obj_MainWindow)
 
     # TEST PART|测试部分
-    gui_MainWindow.tableWidget.setRowCount(
-        len(sceneryCfgContent.sections()) - 1)
-    for i in range(gui_MainWindow.tableWidget.rowCount()):
-        for j in range(gui_MainWindow.tableWidget.columnCount()):
-            if j == 0:
-                tableItemDict[columnContList[j]].insert(
-                    i, TableCheckBoxWidget())
-                gui_MainWindow.tableWidget.setCellWidget(
-                    i, j, tableItemDict[columnContList[j]][i])
-            else:
-                tableItemDict[columnContList[j]].insert(i, QTableWidgetItem())
-                tableItemDict[columnContList[j]][i].setText(sceneryCfgContent.get(
-                    (sceneryCfgContent.sections())[i + 1], columnContList[j]))
-                gui_MainWindow.tableWidget.setItem(
-                    i, j, tableItemDict[columnContList[j]][i])
+    # gui_MainWindow.tableWidget.setRowCount(
+    #     len(sceneCfgCont.sections()) - 1)
+    # for i in range(gui_MainWindow.tableWidget.rowCount()):
+    #     for j in range(gui_MainWindow.tableWidget.columnCount()):
+    #         if j == 0:
+    #             tableItemDict[columnContList[j]].insert(
+    #                 i, TableCheckBoxWidget())
+    #             gui_MainWindow.tableWidget.setCellWidget(
+    #                 i, j, tableItemDict[columnContList[j]][i])
+    #         else:
+    #             tableItemDict[columnContList[j]].insert(i, QTableWidgetItem())
+    #             tableItemDict[columnContList[j]][i].setText(sceneCfgCont.get(
+    #                 (sceneCfgCont.sections())[i + 1], columnContList[j]))
+    #             gui_MainWindow.tableWidget.setItem(
+    #                 i, j, tableItemDict[columnContList[j]][i])
+    print(P3dInfoSrc.getInstedVerList())
     # TEST PART|测试部分
     obj_MainWindow.show()
     sys.exit(app_MainWindow.exec())
